5cad3d6fd2e64246c8940f243d65d2b00fc63a8e
dilyarakhabibullina <khdilyara@rambler.ru>
2019-02-28 22:55:30 +0300

    failed test but I've changed code (made it without Main Method)

--- src/main/java/ru/itpark/Sportmaster.java
+++ src/main/java/ru/itpark/Sportmaster.java
@@ -1,16 +1,17 @@
 package ru.itpark;
 
-public class Main {
-    public static void main(String[] args) {
-            int totalPurchases=15_000;
+public class Sportmaster {
+    public int calculateBonus(int totalPurchases) {
+            //int totalPurchases=15_000;
             int currentPurchase = 4_000;
             int sumPurchase = totalPurchases + currentPurchase;
+            return sumPurchase;
             //int bonus = currentPurchase/1000;
             //int bonusOne = bonus * 100;
 
             //if (sumPurchase>150000){
             //return bonusOne;}
-        System.out.println(sumPurchase);
+
         }
     }
 
