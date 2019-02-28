d3d0dfcc70dbc417e5a96b7c21ccb1d84f426653
dilyarakhabibullina <khdilyara@rambler.ru>
2019-02-28 22:33:06 +0300

    failed test

---
+++ src/main/java/ru/itpark/Main.java
@@ -0,0 +1,16 @@
+package ru.itpark;
+
+public class Main {
+    public static void main(String[] args) {
+            int totalPurchases=15_000;
+            int currentPurchase = 4_000;
+            int sumPurchase = totalPurchases + currentPurchase;
+            //int bonus = currentPurchase/1000;
+            //int bonusOne = bonus * 100;
+
+            //if (sumPurchase>150000){
+            //return bonusOne;}
+        System.out.println(sumPurchase);
+        }
+    }
+
