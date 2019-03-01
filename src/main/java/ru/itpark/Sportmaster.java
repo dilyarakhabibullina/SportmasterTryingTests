6af0513936851299d8bc5d80dce7536931e0e7af
dilyarakhabibullina <khdilyara@rambler.ru>
2019-03-01 11:16:35 +0300

    again test ok after renaming of project

--- src/main/java/ru/itpark/Sportmaster.java
+++ src/main/java/ru/itpark/Sportmaster.java
@@ -1,17 +1,12 @@
 package ru.itpark;
 
 public class Sportmaster {
-    public int calculateBonus(int totalPurchases) {
-            //int totalPurchases=15_000;
-            int currentPurchase = 4_000;
-            int sumPurchase = totalPurchases + currentPurchase;
-            return sumPurchase;
-            //int bonus = currentPurchase/1000;
-            //int bonusOne = bonus * 100;
-
-            //if (sumPurchase>150000){
-            //return bonusOne;}
-
-        }
+    public int calculateBonus(int totalPurchases){
+        //if (monthCosts <= 0) {
+          //  return 0;
+        //}
+        int currentPurchase = 4000;
+        int sumPurchase = totalPurchases + currentPurchase;
+        return sumPurchase;
     }
-
+}
